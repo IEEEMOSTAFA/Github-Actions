dcb33960603464f8b55f1dd158afdb56196b4a76
Md Al Mostafa <skmostafa8888@gmail.com>
2025-06-05 07:18:33 +0600

    feat: added tests and test workflow

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const port = 3000
-app.get('/', (req, res) =>{
-    res.send('Hello World1');
+// const port = 3000
+// app.get('/', (req, res) =>{
+//     res.send('Hello World1');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+
+
+
+
+const http = require('http');
+const app = require('./app'); // Adjust the path to your app file
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
